ce09d9918932048e3577426f1edeeda19be30529
glenn3369 <glenn.maina@strathmore.edu>
2023-06-22 12:08:43 +0300

    added password viewer in login

--- app/Http/Controllers/Admin/SystemUsers.php
+++ app/Http/Controllers/Admin/SystemUsers.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Mail\VerificationEmail;
 use App\Models\User as ModelsUser;
+use Spatie\Permission\Models\Role;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
